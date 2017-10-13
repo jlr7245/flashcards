6904e71104a52850b0e1117e10ad3edf7e630ee1
J Silverstein <jess.pebble@gmail.com>
2017-10-13 14:19:34 -0400

    set up routes for views

--- routes/flashcards-routes.js
+++ routes/flashcards-routes.js
@@ -4,11 +4,16 @@ const flashcardsRouter = express.Router();
 const flashcardsController = require('../controllers/flashcards-controller');
 
 flashcardsRouter.get('/', flashcardsController.index);
-flashcardsRouter.get('/:id', flashcardsController.show);
 
+flashcardsRouter.get('/new', (req, res) => {
+  res.render('flashcards/flashcards-new');
+});
 flashcardsRouter.post('/', flashcardsController.create);
 
+flashcardsRouter.get('/:id', flashcardsController.show);
+flashcardsRouter.get('/:id/edit', flashcardsController.edit);
 flashcardsRouter.put('/:id', flashcardsController.update);
+
 flashcardsRouter.delete('/:id', flashcardsController.delete);
 
 module.exports = flashcardsRouter;
