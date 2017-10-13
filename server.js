abd47e49e81a74d2f233d07846c9a9d83ef91984
J Silverstein <jess.pebble@gmail.com>
2017-10-13 15:50:41 -0400

    add middleware for auth

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,6 +24,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
+app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
