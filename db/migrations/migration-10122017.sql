9362cd7001bef2df9dec581eae55ca79a9dc794f
J Silverstein <jess.pebble@gmail.com>
2017-10-13 12:22:40 -0400

    fix migration to add index

--- db/migrations/migration-10122017.sql
+++ db/migrations/migration-10122017.sql
@@ -1,3 +1,10 @@
 CREATE TABLE IF NOT EXISTS flashcards (
-  
-)
+  id SERIAL PRIMARY KEY,
+  question TEXT,
+  answer TEXT,
+  category VARCHAR(255),
+  difficulty BIGINT
+);
+
+CREATE INDEX idx_category ON flashcards (category);
+CREATE INDEX idx_difficulty ON flashcards (difficulty);
