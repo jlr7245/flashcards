97101d56306d98a949ec2ec2f4a4b556baf240ac
J Silverstein <jess.pebble@gmail.com>
2017-10-30 20:12:28 -0400

    import fetch, add keyword helpers

---
+++ services/keywords/keyword-helpers.js
@@ -0,0 +1,34 @@
+require('isomorphic-fetch');
+
+function getKeywords(req, res, next) {
+  fetch('https://apiv2.indico.io/keywords?version=2', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      api_key: process.env.NODE_ENV,
+      data: req.body.question,
+    }),
+  })
+    .then(fetchRes => fetchRes.json())
+    .then(jsonRes => {
+      res.locals.initialData = jsonRes.results;
+      next();
+    })
+    .catch(err => next(err));
+}
+
+function formatApiData(req, res, next) {
+  const keywords = [];
+  for (let key in res.locals.initialData) {
+    keywords.push(key);
+  }
+  res.locals.keywords = keywords;
+  next();
+}
+
+module.exports = {
+  getKeywords,
+  formatApiData,
+};
