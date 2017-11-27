0a727085dd7978e81665251f047ebe4414533f3d
J Silverstein <jess.pebble@gmail.com>
2017-11-27 16:44:18 -0500

    first pass at fancy models

---
+++ models/Flashcard-protos.js
@@ -0,0 +1,36 @@
+// utilitarian methods
+const utils = {
+  modify: function(changes) {
+    return Object.assign(this, changes);
+  },
+  validate: (property, propType) => {
+    if (property) return property;
+    else throw new Error(`Missing property ${propType}`);
+  },
+}
+
+// default model methods
+const modelDefaults = (tablename) => ({
+  findAll: () => console.log(`SELECT * FROM ${tablename}`),
+  findById: (id) => console.log(`SELECT * FROM ${tablename} WHERE id = ${id}`),
+  destroy: (id) => console.log(`DELETE FROM ${tablename} WHERE id = ${id}`),
+});
+
+// our Flashcard constructor
+function Flashcard(card) {
+  this.question = this.validate(card.question, 'question');
+  this.answer = this.validate(card.answer, 'answer');
+}
+
+// attach the default model methods as static methods
+// and the utility 
+Flashcard.__proto__ = modelDefaults('flashcards');
+Flashcard.prototype = Object.assign(Flashcard.prototype, utils);
+
+
+// and now we can do all these things
+Flashcard.findAll(); // `SELECT * FROM flashcards`
+const newFlashcard = new Flashcard({ question: 'aaa', answer: 'aaa' });
+newFlashcard.modify({ question: 'lsdjfslkdfjlksfd' });
+console.log(newFlashcard); // Flashcard { question: 'lsdjfslkdfjlksfd', answer: 'aaa' }
+
