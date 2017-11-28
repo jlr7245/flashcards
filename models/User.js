fbc0cf7a85d92bce4d60892665bce2b5fa413d0c
J Silverstein <jess.pebble@gmail.com>
2017-11-28 15:05:23 -0500

    convert user to prototypes

--- models/User.js
+++ models/User.js
@@ -1,45 +1,43 @@
 const db = require('../db/config');
 
-class User {
-  constructor(user) {
-    this.id = user.id;
-    this.username = user.username;
-    this.password_digest = user.password_digest;
-    this.email = user.email;
-  }
-
-  static findByUserName(username) {
-    return db.one(`
-      SELECT * FROM users
-      WHERE username = $1
-      ORDER BY id ASC;
-    `, username)
-      .then(user => new User(user));
-  }
-
-  save() {
-    return db.one(`
-      INSERT INTO users (
-        username, email, password_digest
-      ) VALUES (
-        $/username/, $/email/, $/password_digest/
-      )
-      RETURNING *
-    `, this)
-      .then(user => this.modify(user));
-  }
-
-  modify(changes) {
-    return Object.assign(this, changes);
-  }
-
-  flashcards() {
-    return db.manyOrNone(`
-      SELECT *
-      FROM flashcards
-      WHERE user_id = $/id/
-    `, this);
-  }
+function User(user) {
+  this.id = user.id || null;
+  this.username = this.validate(user.username, 'username');
+  this.password_digest = this.validate(user.password_digest, 'password_digest');
+  this.email = this.validate(user.email, 'email');
 }
+
+const userStatics = require('./model-defaults')('users');
+userStatics.findByUserName = (username) => {
+  return db.one(`
+    SELECT * FROM users
+    WHERE username = $1
+    ORDER BY id ASC
+  `, username)
+}
+Object.setPrototypeOf(User, userStatics);
+
+User.prototype = Object.assign(User.prototype, require('./utils'));
+
+User.prototype.save = function() {
+  return db.one(`
+    INSERT INTO users (
+      username, email, password_digest
+    ) VALUES (
+      $/username/, $/email/, $/password_digest/
+    )
+    RETURNING *
+  `, this)
+  .then(user => this.modify(user));
+}
+
+User.prototype.flashcards = function() {
+  return db.manyOrNone(`
+    SELECT *
+    FROM flashcards
+    WHERE user_id = $/id/
+  `, this);
+}
+
 
 module.exports = User;
