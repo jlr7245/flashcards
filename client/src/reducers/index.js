a19abb51bbeb4f928b0a53ac8f82284af7a549b9
J Silverstein <jess.pebble@gmail.com>
2018-01-17 11:11:27 -0500

    remove CRA setup

--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { flashcards, flashcardsHasErrored, flashcardsIsLoading } from './flashcards';
-
-export default combineReducers({
-  flashcards,
-  flashcardsHasErrored,
-  flashcardsIsLoading,
-});
-
